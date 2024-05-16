98c058693f0846ea2a833ec724130bbc089898b6
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-05-16 09:07:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Challenge_Lab_1_with_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature in Fahrenheit: ");
+            double temperature = Convert.ToDouble(Console.ReadLine());
+
+            string weatherMessage;
+
+            
+            
+
+
+                switch (temperature)
+                {
+                    case double temp when temp < 10:
+                        weatherMessage = "Freezing weather";
+                        break;
+                    case double temp when temp >= 10 && temp <= 20:
+                        weatherMessage = "Very Cold weather";
+                        break;
+                    case double temp when temp > 20 && temp <= 35:
+                        weatherMessage = "Cold weather";
+                        break;
+                    case double temp when temp > 35 && temp <= 50:
+                        weatherMessage = "Normal in Weather";
+                        break;
+                    case double temp when temp > 50 && temp <= 65:
+                        weatherMessage = "It's Hot";
+                        break;
+                    case double temp when temp > 65 && temp <= 80:
+                        weatherMessage = "It's Very Hot";
+                        break;
+                    default:
+                        weatherMessage = "Unknown weather condition";
+                        break;
+                }
+
+                Console.WriteLine($"Weather condition: {weatherMessage}");
+            
+            
+        }
+    }
+}
